36e44d41a1e37ec53d93b4ea4f2be789aec9452d
Max Kolotilkin <kolotilkin@gmail.com>
2019-08-28 22:03:15 +0300

    App class. Home controller. App properties file

---
+++ src/main/java/com/kolotilkin/users/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.kolotilkin.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "{status:'up'}";
+    }
+}
