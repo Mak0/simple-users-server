bc95a5cec8d367540cb2df356579700281b5a7a3
Max Kolotilkin <kolotilkin@gmail.com>
2019-08-29 09:16:48 +0300

    Added random joke to home controller

--- src/main/java/com/kolotilkin/users/controllers/HomeController.java
+++ src/main/java/com/kolotilkin/users/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.kolotilkin.users.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HomeController {
@@ -9,5 +10,11 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "{status:'up'}";
+    }
+
+    @GetMapping("/joke")
+    public String joke() {
+        RestTemplate template = new RestTemplate();
+        return template.getForObject("http://api.icndb.com/jokes/random", String.class);
     }
 }
